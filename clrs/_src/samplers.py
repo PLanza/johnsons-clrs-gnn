dcd23b5504b194672a4ea090b5d597ae8011d55f
Pablo Lanza <pablolanza89@gmail.com>
2024-02-14 18:57:07 +0000

    Applied Vik's fix to the sampler

--- clrs/_src/samplers.py
+++ clrs/_src/samplers.py
@@ -524,7 +524,7 @@ class JohnsonsSampler(Sampler):
       self,
       length: int,
       p: Tuple[float, ...] = (0.5,),
-      low: float = -1.,
+      low: float = -0.5,
       high: float = 1.,
   ):
     graph = self._random_er_graph(
